0169dabfcf69f796aad97796b5dacadd5f74d26d
Joseph Segbefia <elorma.segbefia@gmail.com>
2024-06-04 03:08:45 +0200

    Halfway home page

--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -1,11 +1,19 @@
 import { View, Text } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import ExchangeRateCard from '../../components/ExchangeRateCard';
 import React from 'react';
 
 const Home = () => {
   return (
-    <View>
-      <Text>Home</Text>
-    </View>
+    <SafeAreaView className='h-full bg-primary-50  items-center'>
+      <View className='justify-center text-center w-[90%] mt-20'>
+        <ExchangeRateCard
+          title='Current Rate'
+          hostCountryId='de'
+          recipientCountryId='gh'
+        />
+      </View>
+    </SafeAreaView>
   );
 };
 
---
+++ components/ExchangeRateCard.jsx
@@ -0,0 +1,51 @@
+import { View, Text, TouchableOpacity } from 'react-native';
+import { router } from 'expo-router';
+import CountryFlag from 'react-native-country-flag';
+import { FontAwesome } from '@expo/vector-icons';
+import CustomButton from './CustomButton';
+
+const ExchangeRateCard = ({ title, hostCountryId, recipientCountryId }) => {
+  return (
+    <View className='border border-primary rounded-xl'>
+      <Text className='text-center text-primary font-psemibold text-sm py-5'>
+        {title}
+      </Text>
+      <View className='flex-row justify-around'>
+        <View>
+          <CountryFlag
+            isoCode={hostCountryId}
+            size={50}
+            className='border rounded-full w-[70px] h-[70px]'
+          />
+          <Text className='text-center mt-3 text-primary font-pbold'>
+            1.00 EUR
+          </Text>
+        </View>
+        <View className='mt-5'>
+          <FontAwesome name='exchange' color='#004d40' size={30} />
+        </View>
+        <View>
+          <CountryFlag
+            isoCode={recipientCountryId}
+            size={50}
+            className='border rounded-full w-[70px] h-[70px]'
+          />
+          <Text className='text-center mt-3 text-primary font-pbold'>
+            17.00 GHS
+          </Text>
+        </View>
+      </View>
+      <View className='items-center py-5'>
+        <TouchableOpacity
+          onPress={() => router.replace('/send')}
+          activeOpacity={0.3}
+          className='bg-primary-200 border-primary border rounded-full justify-center items-center w-[80%] py-1'
+        >
+          <Text className='text-primary text-xl font-psemibold'>Send Now</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+};
+
+export default ExchangeRateCard;
