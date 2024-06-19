4560133dd616265fe6de8bcc956349222a00d8b7
Joseph Segbefia <elorma.segbefia@gmail.com>
2024-06-19 11:36:26 +0200

    Finish search functionality on transfers screen

--- app/(tabs)/transfers.jsx
+++ app/(tabs)/transfers.jsx
@@ -1,6 +1,12 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useCallback } from 'react';
 import { SafeAreaView } from 'react-native-safe-area-context';
-import { View, Text, FlatList, StyleSheet } from 'react-native';
+import {
+  View,
+  Text,
+  FlatList,
+  StyleSheet,
+  TouchableOpacity,
+} from 'react-native';
 import EmptyState from '../../components/EmptyState';
 import CustomCard from '../../components/CustomCard';
 import { useGlobalContext } from '../../context/GlobalProvider';
@@ -8,11 +14,49 @@ import useAppwrite from '../../lib/useAppwrite';
 import { getTransfers } from '../../lib/appwrite';
 import LoadingOverlay from '../../components/LoadingOverlay';
 import formatDate from '../../lib/formatDate';
+import { useNavigation } from 'expo-router';
+import FormField from '../../components/FormField';
 
 const Transfers = () => {
+  const [searchValue, setSearchValue] = useState('');
+  const [filteredTransfers, setFilteredTransfers] = useState([]);
   const { user } = useGlobalContext();
   const { data: transfers, isLoading: isLoading } = useAppwrite(() =>
     getTransfers(user.$id)
+  );
+
+  const navigation = useNavigation();
+
+  useEffect(() => {
+    if (transfers) {
+      setFilteredTransfers(
+        transfers.filter((transfer) =>
+          transfer.recipient.firstName
+            .toLowerCase()
+            .includes(searchValue.toLowerCase())
+        )
+      );
+    }
+  }, [searchValue, transfers]);
+
+  const renderItem = useCallback(
+    ({ item }) => (
+      <View className='w-full items-center'>
+        <View className='w-[95%]'>
+          <TouchableOpacity onPress={() => {}} activeOpacity={0.3}>
+            <CustomCard
+              firstName={item.recipient.firstName}
+              lastName={item.recipient.lastName}
+              date={formatDate(item.$createdAt)}
+              status={item.status}
+              amount={item.receivableAmount}
+              isTransferHistory
+            />
+          </TouchableOpacity>
+        </View>
+      </View>
+    ),
+    [navigation]
   );
 
   if (isLoading) {
@@ -21,36 +65,26 @@ const Transfers = () => {
 
   return (
     <SafeAreaView className='h-full bg-primary-50 flex-1'>
+      <View className='my-3 px-4'>
+        <View className='justify-between items-start flex-row mb-2'>
+          <View>
+            <Text className='mt-10 text-primary text-2xl font-pbold'>
+              Transfers
+            </Text>
+          </View>
+        </View>
+        <View>
+          <FormField
+            placeholder='Search transfers by recipient first name'
+            value={searchValue}
+            handleChangeText={(e) => setSearchValue(e)}
+          />
+        </View>
+      </View>
       <FlatList
-        data={transfers}
+        data={filteredTransfers}
         keyExtractor={(item) => item.$id}
-        renderItem={({ item }) => (
-          <View className='w-full items-center'>
-            <View className='mt-2 w-[95%]'>
-              <CustomCard
-                firstName={item.recipient.firstName}
-                lastName={item.recipient.lastName}
-                date={formatDate(item.$createdAt)}
-                status={item.status}
-                amount={item.receivableAmount}
-                isTransferHistory
-              />
-            </View>
-          </View>
-        )}
-        stickyHeaderIndices={[0]}
-        ListHeaderComponentStyle={styles.header}
-        ListHeaderComponent={() => (
-          <View className='my-3 px-4 space-y-6'>
-            <View className='justify-between items-start flex-row mb-6'>
-              <View>
-                <Text className='mt-10 text-primary text-2xl font-pbold'>
-                  Transfers
-                </Text>
-              </View>
-            </View>
-          </View>
-        )}
+        renderItem={renderItem}
         ListEmptyComponent={() => (
           <EmptyState
             title='No transfers found'
