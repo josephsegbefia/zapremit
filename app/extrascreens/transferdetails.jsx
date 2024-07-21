a5b5328489936969a4663dc9b3dc4d7812697d24
Joseph Segbefia <elorma.segbefia@gmail.com>
2024-07-21 23:21:29 +0200

    Start transfer details screen

---
+++ app/extrascreens/transferdetails.jsx
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+
+const TransferDetails = () => {
+  return (
+    <View>
+      <Text>transferdetails</Text>
+    </View>
+  );
+};
+
+export default TransferDetails;
